2a616b10dfb79e4cb95c205f1b0fbc60e36acb24
AmaliaIka <amaliaikasriyani1840@gmail.com>
2024-09-23 11:17:55 +0800

    Update week-4-second.py

--- week-4-second.py
+++ week-4-second.py
@@ -5,55 +5,50 @@ sx = st.text_input('Satuan', 'C')
 st.write("Anda memasukkan", x, ' ', sx)
 sy = st.text_input('Dikonversi ke', 'C')
 y = 0
-# def konversi( ):
-	if (sx=='C'):
-		if (sy=='C'):
-			y = x
-		elif (sy=='R'):
-			y = (x*4/5)
-		elif (sy=='F'):
-			y = ((x+32)*5/9)
-		elif (sy=='K'):
-			y = (x+273)
-		else:
-			'masukkan nya salah'
-	elif (sx=='R'):
-		if (sy=='C'):
-			y = (x*5/4)
-		elif (sy=='R'):
-			y = x
-		elif (sy=='F'):
-			y = ((x+32)*4/9)
-		elif (sy=='K'):
-			y = (x*5/4+273)
-		else:
-			'masukkan nya salah'
-	elif (sx=='F'):
-		if (sy=='C'):
-			y = ((x+32)*9/5)
-		elif (sy =='R'):
-			y = ((x+32)*4/5)
-		elif (sy=='F'):
-			y = x
-		elif (sy=='K'):
-			y = (((x+32)*5/9)+273)
-		else:
-			'masukkan nya salah'
-	elif (sx=='K'):
-		if (sy=='C'):
-			y = (x-273)
-		elif (sy=='R'):
-			y = ((x-273)*4/5)
-		elif (sy=='F'):
-			y = ((x-273)*9/5+32)
-		elif (sy=='K'):
-			y = x
-		else:
-			'masukkan nya salah'
-	#else:
-			'masukkan nya salah'
+
+if (sx == 'C'):
+	if (sy == 'C'):
+		y = x
+	elif (sy == 'R'):
+		y = (x*4/5)
+	elif (sy == 'F'):
+		y = ((x+32)*5/9)
+	elif (sy == 'K'):
+		y = (x+273)
+	else:
+		'masukkan nya salah'
+if (sx == 'R'):
+	if (sy == 'C'):
+		y = (x*5/4)
+	elif (sy == 'R'):
+		y = x
+	elif (sy == 'F'):
+		y = ((x+32)*4/9)
+	elif (sy == 'K'):
+		y = (x*5/4+273)
+	else:
+		'masukkan nya salah'
+if (sx == 'F'):
+	if (sy == 'C'):
+		y = ((x+32)*9/5)
+	elif (sy == 'R'):
+		y = ((x+32)*4/5)
+	elif (sy == 'F'):
+		y = x
+	elif (sy == 'K'):
+		y = (((x+32)*5/9)+273)
+	else:
+		'masukkan nya salah'
+if (sx == 'K'):
+	if (sy == 'C'):
+		y = (x-273)
+	elif (sy == 'R'):
+		y = ((x-273)*4/5)
+	elif (sy=='F'):
+		y = ((x-273)*9/5+32)
+	elif (sy=='K'):
+		y = x
+	else:
+		'masukkan nya salah'
 		
-#def penutup( ):
-	#print('selesai')
-  
 st.write(x, ' ', sx, ' = ', y, sy)
