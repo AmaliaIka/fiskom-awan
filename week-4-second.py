7a6b2120d44fe4cbd593f3759679b343af0f1943
AmaliaIka <amaliaikasriyani1840@gmail.com>
2024-09-23 11:10:38 +0800

    Update week-4-second.py

--- week-4-second.py
+++ week-4-second.py
@@ -4,54 +4,54 @@ x = st.number_input('Masukkan angka')
 sx = st.text_input('Satuan', 'C')
 st.write("Anda memasukkan", x, ' ', sx)
 sy = st.text_input('Dikonversi ke', 'C')
-
+y = 0
 def konversi( ):
-	if (dari=='C'):
-		if (ke=='C'):
-			print(x)
-		elif (ke=='R'):
-			print (x*4/5)
-		elif (ke=='F'):
-			print ((x+32)*5/9)
-		elif (ke=='K'):
-			print (x+273)
+	if (sx=='C'):
+		if (sy=='C'):
+			y = x
+		elif (sy=='R'):
+			y = (x*4/5)
+		elif (sy=='F'):
+			y = ((x+32)*5/9)
+		elif (sy=='K'):
+			y = (x+273)
 		else:
-			print('masukkan nya salah')	
-	elif (dari=='R'):
-		if (ke=='C'):
-			print(x*5/4)
-		elif (ke=='R'):
-			print(x)
-		elif (ke=='F'):
-			print((x+32)*4/9)
-		elif (ke=='K'):
-			print(x*5/4+273)
+			'masukkan nya salah'
+	elif (sx=='R'):
+		if (sy=='C'):
+			y = (x*5/4)
+		elif (sy=='R'):
+			y = x
+		elif (sy=='F'):
+			y = ((x+32)*4/9)
+		elif (sy=='K'):
+			y = (x*5/4+273)
 		else:
-			print('masukkan nya salah')	
-	elif (dari=='F'):
-		if (ke=='C'):
-			print((x+32)*9/5)
-		elif (ke=='R'):
-			print((x+32)*4/5)
-		elif (ke=='F'):
-			print(x)
-		elif (ke=='K'):
-			print(((x+32)*5/9)+273)
+			'masukkan nya salah'
+	elif (sx=='F'):
+		if (sy=='C'):
+			y = ((x+32)*9/5)
+		elif (sy =='R'):
+			y = ((x+32)*4/5)
+		elif (sy=='F'):
+			y = x
+		elif (sy=='K'):
+			y = (((x+32)*5/9)+273)
 		else:
-			print('masukkan nya salah')
-	elif (dari=='K'):
-		if (ke=='C'):
-			print(x-273)
-		elif (ke=='R'):
-			print((x-273)*4/5)
-		elif (ke=='F'):
-			print((x-273)*9/5+32)
-		elif (ke=='K'):
-			print(x)
+			'masukkan nya salah'
+	elif (sx=='K'):
+		if (sy=='C'):
+			y = (x-273)
+		elif (sy=='R'):
+			y = ((x-273)*4/5)
+		elif (sy=='F'):
+			y = ((x-273)*9/5+32)
+		elif (sy=='K'):
+			y = x
 		else:
-			print('masukkan nya salah')
+			'masukkan nya salah'
 	else:
-			print('masukkan nya salah')
+			'masukkan nya salah'
 		
 #def penutup( ):
 	#print('selesai')
