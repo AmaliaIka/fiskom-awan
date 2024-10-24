30536ffec2d34872dad27e7676a8fb62856711d3
AmaliaIka <amaliaikasriyani1840@gmail.com>
2024-10-24 11:36:11 +0800

    Update utsfka.py
    thirdchange

--- utsfka.py
+++ utsfka.py
@@ -8,32 +8,26 @@ from matplotlib.patches import Circle
 st.title(':sparkles: UTS Fisika Komputasi Awan :sparkles:')
 
 # Header
-st.header('Amalia Ika Sriyani')
-st.header('210322607258')
+st.header('Amalia Ika Sriyani/210322607258')
 
 # Membuat lingkaran utama dengan radius 1
-circle = Circle((0, 0), 1, color = 'black', fill = False, linewidth = 2, linestyle = '-', alpha = 0.2)
+circle = Circle((0, 0), 1, color = 'black', fill = False, linewidth = 3, linestyle = '-', alpha = 0.3)
 
-# Mmembuat variabel untuk menyimpan titik
-x = []
-y = []
-color = []
-size = []
-
-# Titik pusat awal lingkaran
-x.append(0)
-y.append(0)
-color.append((0.,.7,0.))
-size.append(371)
+# Inisialisasi list untuk menyimpan titik
+x = [0]
+y = [0]
+color = [0.0, 0.7, 0.0]
+size = [371]
 
 # Membuat tombol untuk menghasilkan data acak
 if st.button("Data"):
     for i in range(111):
+        # Menghasilkan x, y acak dalam rentang [-1, 1]
         x0 = 2*(random.random() - .5)
         y0 = 2*(random.random() - .5)
         # Memeriksa apakah titik berada di dalam lingkaran utama 
-        if ((x0**2+y0**2)>1.):
-            if y0>0:
+        if ((x0**2+y0**2) > 1.):
+            if y0 > 0:
                 y0 = np.sqrt(1-x0**2)
             else:
                 y0 = -1*(np.sqrt(1-x0**2))
@@ -41,30 +35,29 @@ if st.button("Data"):
         # Menambahkan titik baru    
         x.append(x0)
         y.append(y0)
-        color.append((random.random(),random.random(),random.random()))
+        color.append((random.random(), random.random(), random.random()))
         size.append(3713*random.random() )
 
 # Membuat figure
-fig, ax = plt.subplots(figsize=(16, 16))
+fig, ax = plt.subplots(figsize = (16, 16))
 ax.add_patch(circle)
 
 # Menggambar garis dari pusat ke titik
 for i in range(1, len(x)):
-    ax.plot([0, x[i]], [0, y[i]], color='green', linestyle='--', alpha=0.2)
+    ax.plot([0, x[i]], [0, y[i]], color = 'black', linestyle = '--', alpha = 0.3)
 
-# Plot untuk titik
-ax.scatter(x, y, c=color, s=size, alpha=0.5) 
+# Plot scatter untuk titik
+ax.scatter(x, y, c = color, s = size, alpha = 0.5) 
 
 # Membuat plot 
 ax.set_ylabel("y")
 ax.set_xlabel("x")
-ax.tick_params(axis='y', labelsize=20)
-ax.tick_params(axis='x', labelsize=15)
-ax.set_title('Data Acak yang berubah setiap tombol ditekan')
-ax.grid(True, linestyle='-.')
-ax.tick_params(labelcolor='r', labelsize='medium', width=3)
+ax.tick_params(axis = 'y', labelsize = 20)
+ax.tick_params(axis = 'x', labelsize = 20)
+ax.set_title('Data Acak yang Berubah Setiap Tombol Ditekan')
+ax.grid(True, linestyle = '-.')
+ax.tick_params(labelcolor = 'r', labelsize = 'medium', width = 3)
 ax.set_xlim([-1, 1])
 ax.set_ylim([-1, 1])
 st.pyplot(fig)
 st.caption("Lingkaran dengan ukuran dan warna acak dan tersebar didalam lingkaran dengan radius 1")
-st.divider()
